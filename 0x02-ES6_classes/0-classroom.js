667cfa05b4e889487f93e7f0a546055ce09a4245
VICTOR-OLULETI <oluletiv@gmail.com>
2023-09-29 03:38:22 +0100

    defining class Classroom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Create class room
+ */
+export default class ClassRoom {
+  /**
+     *
+     * @param {Number} maxStudentsSize - maximum numer of
+     * students
+     */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
