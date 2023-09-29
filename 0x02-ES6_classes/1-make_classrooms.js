05cc6280459b9eb1d2f0a0b19f3a9344aecf216d
VICTOR-OLULETI <oluletiv@gmail.com>
2023-09-29 21:37:23 +0100

    initialize classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -5,5 +5,5 @@ import ClassRoom from './0-classroom';
  * @returns An array of {@link ClassRoom}s.
  */
 export default function initializeRooms() {
-  return [10, 20, 34].map((size) => new ClassRoom(size));
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
