b86e970b243af12f152dc0e68a87a644d5171316
VICTOR-OLULETI <oluletiv@gmail.com>
2023-10-04 04:52:39 +0100

    use filter to return specific objects

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {Array} students : list of student objects 
+ * @param {String} location : specific city
+ * @returns (an array of objects located in a specific city)
+ */
+export default function getStudentsByLocation(students, location) {
+    if (students instanceof Array) {
+        return (students.filter((student) => student.location == location))
+    }
+}
